b75c0a63ff3ba7744609b07d34336fbd51fdf7c6
Bakhtiyar Kussainov <bakhtiyar_kussainov@epam.com>
2023-09-28 22:41:04 +0500

    init

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char[] characters = {'c', 'h', 'a', 'r'};
+        for (char c : characters) {
+            int intValue = (int) c;
+            System.out.println(intValue);
+        }
     }
 }
